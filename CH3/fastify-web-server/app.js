573e86c822da3f0e184bf81a226a90ac823dd0de
ak <41680455+akudrin@users.noreply.github.com>
2022-06-16 16:21:00 +0100

    Streaming Content with Fastify

--- CH3/fastify-web-server/app.js
+++ CH3/fastify-web-server/app.js
@@ -3,16 +3,16 @@
 const path = require("path");
 const AutoLoad = require("@fastify/autoload");
 
-const dev = process.env.NODE_ENV !== "production";
-
-const fastifyStatic = dev && require("@fastify/static");
+const pointOfView = require("point-of-view");
+const handlebars = require("handlebars");
 
 module.exports = async function (fastify, opts) {
-  if (dev) {
-    fastify.register(fastifyStatic, {
-      root: path.join(__dirname, "public"),
-    });
-  }
+  fastify.register(pointOfView, {
+    engine: { handlebars },
+    root: path.join(__dirname, "views"),
+    layout: "layout.hbs",
+  });
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, "plugins"),
     options: Object.assign({}, opts),
@@ -22,6 +22,7 @@ module.exports = async function (fastify, opts) {
     dir: path.join(__dirname, "routes"),
     options: Object.assign({}, opts),
   });
+
   fastify.setNotFoundHandler((request, reply) => {
     if (request.method !== "GET") {
       reply.status(405);
