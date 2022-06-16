39c7acd0448712776d164990f759e5c21f3ab48a
ak <41680455+akudrin@users.noreply.github.com>
2022-06-16 10:21:40 +0100

    Serving Static Content with Fastify

--- CH3/fastify-web-server/app.js
+++ CH3/fastify-web-server/app.js
@@ -3,29 +3,30 @@
 const path = require("path");
 const AutoLoad = require("@fastify/autoload");
 
+const dev = process.env.NODE_ENV !== "production";
+
+const fastifyStatic = dev && require("@fastify/static");
+
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
+  if (dev) {
+    fastify.register(fastifyStatic, {
+      root: path.join(__dirname, "public"),
+    });
+  }
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
   fastify.setNotFoundHandler((request, reply) => {
     if (request.method !== "GET") {
       reply.status(405);
       return "Method Not Allowed\n";
     }
     return "Not Found\n";
-  });
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
-    options: Object.assign({}, opts),
-  });
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "routes"),
-    options: Object.assign({}, opts),
   });
 };
