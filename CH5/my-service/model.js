515af70a737ec2435cc6c497472f3ced2ff2ffbf
ak <41680455+akudrin@users.noreply.github.com>
2022-06-24 09:52:26 +0100

    Implementing POST, PUT and DELETE with Fastify

--- CH5/my-service/model.js
+++ CH5/my-service/model.js
@@ -11,8 +11,34 @@ function bicycleModel() {
   };
 
   return {
+    create,
     read,
+    update,
+    del,
+    uid,
   };
+
+  function uid() {
+    return (
+      Object.keys(db)
+        .sort((a, b) => a - b)
+        .map(Number)
+        .filter((n) => !isNaN(n))
+        .pop() +
+      1 +
+      ""
+    );
+  }
+
+  function create(id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error("resource exists");
+      setImmediate(() => cb(err));
+      return;
+    }
+    db[id] = data;
+    setImmediate(() => cb(null, id));
+  }
 
   function read(id, cb) {
     if (!db.hasOwnProperty(id)) {
@@ -21,5 +47,25 @@ function bicycleModel() {
       return;
     }
     setImmediate(() => cb(null, db[id]));
+  }
+
+  function update(id, data, cb) {
+    if (!db.hasOwnProperty(id)) {
+      const err = Error("not found");
+      setImmediate(() => cb(err));
+      return;
+    }
+    db[id] = data;
+    setImmediate(() => cb());
+  }
+
+  function del(id, cb) {
+    if (!db.hasOwnProperty(id)) {
+      const err = Error("not found");
+      setImmediate(() => cb(err));
+      return;
+    }
+    delete db[id];
+    setImmediate(() => cb());
   }
 }
--- CH5/my-service/routes/bicycle/index.js
+++ CH5/my-service/routes/bicycle/index.js
@@ -1,15 +1,39 @@
 "use strict";
 const { promisify } = require("util");
 const { bicycle } = require("../../model");
+const { uid } = bicycle;
 const read = promisify(bicycle.read);
+const create = promisify(bicycle.create);
+const update = promisify(bicycle.update);
 
 module.exports = async (fastify, opts) => {
   const { notFound } = fastify.httpErrors;
 
-  fastify.get("/:id", async (request, reply) => {
+  fastify.post("/", async (request, reply) => {
+    const { data } = request.body;
+    const id = uid();
+    await create(id, data);
+    reply.code(201);
+    return { id };
+  });
+
+  fastify.post("/:id/update", async (request, reply) => {
+    const { id } = request.params;
+    const { data } = request.body;
+    try {
+      await update(id, data);
+      reply.code(204);
+    } catch (err) {
+      if (err.message === "not found") throw notFound();
+      throw err;
+    }
+  });
+
+  fastify.delete("/:id", async (request, reply) => {
     const { id } = request.params;
     try {
-      return await read(id);
+      await del(id);
+      reply.code(204);
     } catch (err) {
       if (err.message === "not found") throw notFound();
       throw err;
