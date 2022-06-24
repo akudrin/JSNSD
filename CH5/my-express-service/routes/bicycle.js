ce86f587e36eabdf3db2c7b7cfd41e818ed43c2a
ak <41680455+akudrin@users.noreply.github.com>
2022-06-24 10:17:05 +0100

    Implementing POST, PUT and DELETE with Express

--- CH5/my-express-service/routes/bicycle.js
+++ CH5/my-express-service/routes/bicycle.js
@@ -12,5 +12,51 @@ router.get("/:id", function (req, res, next) {
     }
   });
 });
+router.post("/", function (req, res, next) {
+  var id = model.bicycle.uid();
+  model.bicycle.create(id, req.body.data, (err) => {
+    if (err) next(err);
+    else res.status(201).send({ id });
+  });
+});
+
+router.post("/:id/update", function (req, res, next) {
+  model.bicycle.update(req.params.id, req.body.data, (err) => {
+    if (err) {
+      if (err.message === "not found") next();
+      else next(err);
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+
+router.put("/:id", function (req, res, next) {
+  model.bicycle.create(req.params.id, req.body.data, (err) => {
+    if (err) {
+      if (err.message === "resource exists") {
+        model.bicycle.update(req.params.id, req.body.data, (err) => {
+          if (err) next(err);
+          else res.status(204).send();
+        });
+      } else {
+        next(err);
+      }
+    } else {
+      res.status(201).send({});
+    }
+  });
+});
+
+router.delete("/:id", function (req, res, next) {
+  model.bicycle.del(req.params.id, (err) => {
+    if (err) {
+      if (err.message === "not found") next();
+      else next(err);
+    } else {
+      res.status(204).send();
+    }
+  });
+});
 
 module.exports = router;
