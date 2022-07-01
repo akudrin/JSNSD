2104d901e50674831b5b75d603dfef379d6b1723
ak <41680455+akudrin@users.noreply.github.com>
2022-07-01 10:57:13 +0100

    React16 - Route Validation with Fastify

--- CH5/my-service/routes/bicycle/index.js
+++ CH5/my-service/routes/bicycle/index.js
@@ -5,48 +5,138 @@ const { uid } = bicycle;
 const read = promisify(bicycle.read);
 const create = promisify(bicycle.create);
 const update = promisify(bicycle.update);
+const del = promisify(bicycle.del);
 
 module.exports = async (fastify, opts) => {
   const { notFound } = fastify.httpErrors;
 
-  fastify.get("/:id", async (request, reply) => {
-    const { id } = request.params;
-    try {
-      return await read(id);
-    } catch (err) {
-      if (err.message === "not found") throw notFound();
-      throw err;
-    }
-  });
+  const dataSchema = {
+    type: "object",
+    required: ["brand", "color"],
+    additionalProperties: false,
+    properties: {
+      brand: { type: "string" },
+      color: { type: "string" },
+    },
+  };
 
-  fastify.post("/", async (request, reply) => {
-    const { data } = request.body;
-    const id = uid();
-    await create(id, data);
-    reply.code(201);
-    return { id };
-  });
+  const bodySchema = {
+    type: "object",
+    required: ["data"],
+    additionalProperties: false,
+    properties: {
+      data: dataSchema,
+    },
+  };
 
-  fastify.post("/:id/update", async (request, reply) => {
-    const { id } = request.params;
-    const { data } = request.body;
-    try {
-      await update(id, data);
-      reply.code(204);
-    } catch (err) {
-      if (err.message === "not found") throw notFound();
-      throw err;
-    }
-  });
+  const idSchema = { type: "integer" };
+  const paramsSchema = { id: idSchema };
 
-  fastify.delete("/:id", async (request, reply) => {
-    const { id } = request.params;
-    try {
-      await del(id);
-      reply.code(204);
-    } catch (err) {
-      if (err.message === "not found") throw notFound();
-      throw err;
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: bodySchema,
+        response: {
+          201: {
+            id: idSchema,
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      const { data } = request.body;
+      const id = uid();
+      await create(id, data);
+      reply.code(201);
+      return { id };
     }
-  });
+  );
+
+  fastify.post(
+    "/:id/update",
+    {
+      schema: {
+        body: bodySchema,
+        params: paramsSchema,
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      const { data } = request.body;
+      try {
+        await update(id, data);
+        reply.code(204);
+      } catch (err) {
+        if (err.message === "not found") throw notFound();
+        throw err;
+      }
+    }
+  );
+
+  fastify.get(
+    "/:id",
+    {
+      schema: {
+        params: paramsSchema,
+        response: {
+          200: dataSchema,
+        },
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      try {
+        return await read(id);
+      } catch (err) {
+        if (err.message === "not found") throw notFound();
+        throw err;
+      }
+    }
+  );
+
+  fastify.put(
+    "/:id",
+    {
+      schema: {
+        body: bodySchema,
+        params: paramsSchema,
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      const { data } = request.body;
+      try {
+        await create(id, data);
+        reply.code(201);
+        return {};
+      } catch (err) {
+        if (err.message === "resource exists") {
+          await update(id, data);
+          reply.code(204);
+        } else {
+          throw err;
+        }
+      }
+    }
+  );
+
+  fastify.delete(
+    "/:id",
+    {
+      schema: {
+        params: paramsSchema,
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      try {
+        await del(id);
+        reply.code(204);
+      } catch (err) {
+        if (err.message === "not found") throw notFound();
+        throw err;
+      }
+    }
+  );
 };
